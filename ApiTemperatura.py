3d14fe663f0eb0e3aa57efb8dd62815c0ac9bc6a
Hercules Portela <portela162@gmail.com>
2025-06-18 15:47:14 -0400

    primeiro commit

---
+++ ApiTemperatura.py
@@ -0,0 +1,18 @@
+import requests
+
+def informacao_temperatura_tempo(cidade):
+    api_key = "0545db5bbf97d924eda6b028ede79f6d"
+    link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={api_key}&lang=pt_BR"
+
+    requisicao = requests.get(link)
+    requisicao_dic = requisicao.json()
+
+    if requisicao.status_code == 200:
+        descricao = requisicao_dic['weather'][0]['description']
+        temperatura = requisicao_dic['main']['temp'] - 273.15
+        print(descricao, f"{temperatura}ºC")
+    else:
+        print(f"Não foi possivel entrar a cidade: {cidade}. Erro{requisicao_dic.get('message', 'Desconhecido')}")
+
+
+
---
+++ main.py
@@ -0,0 +1,9 @@
+import ApiTemperatura
+from ApiTemperatura import informacao_temperatura_tempo
+import  tkinter as tk
+
+
+cidade_digitada = input("qual a sua cidade ?: ")
+
+informacao_temperatura_tempo(cidade_digitada)
+
